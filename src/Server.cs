00fda5d26c08de8bc76390cbf603b34776c1996e
Thomas Fenot <fenotthomas@gmail.com>
2024-06-12 16:22:11 +0200

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -6,8 +6,16 @@ using System.Text;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
+var okResponse = "HTTP/1.1 200 OK\r\n\r\n";
+var notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket();
-var response = "HTTP/1.1 200 OK\r\n\r\n";
-socket.Send(Encoding.ASCII.GetBytes(response));
+var acceptedSocket = socket.Accept();
+byte[] buffer = new byte[] { };
+int response = acceptedSocket.Receive(buffer);
+
+Console.WriteLine($"This is the receive response = {response}");
+socket.Send(Encoding.ASCII.GetBytes(okResponse));
